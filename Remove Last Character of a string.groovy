d32ccf02b848f5d585d80fa5981888d65f37858a
Rinaldi-James-Michael <64921836+Rinaldi-James-Michael@users.noreply.github.com>
2023-03-02 10:23:27 +0530

    Create Remove Last Character of a string.groovy
    First commit

---
+++ Remove Last Character of a string.groovy
@@ -0,0 +1,12 @@
+/////////////////Rinaldi Michael///////////////////
+/////////Created in 28th Feb 2023, 07:42 pm////////
+/////Modified in 28th Feb 2023, 07:42 pm///////////
+///////////////////////////////////////////////////
+        
+import java.lang.String
+import java.io.*
+ 
+String text = "123456789"
+text=text[0..printtext.size()-1] //remove the last character
+ 
+return text
